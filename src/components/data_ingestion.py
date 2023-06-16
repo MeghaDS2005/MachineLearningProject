452a879690ff5797f21d25c2941e36e41f5444ab
MeghaDS2005 <1998meghasharma2026@gmail.com>
2023-06-17 01:22:27 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,12 @@
 import os
 import sys
+from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass ## used to create class variables
+from dataclasses import dataclass                                     ## used to create class variables
 
 @dataclass
 class DataIngestionConfig:                                            #artifacts are folders
@@ -45,13 +46,13 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     obj.initiate_data_ingestion()
-         #train_data,test_data=obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-         #data_transformation=DataTransformation()
-         #train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-         #modeltrainer=ModelTrainer()
-         #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+   # modeltrainer=ModelTrainer()
+   # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
    
 
     
